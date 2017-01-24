24a00535ea8431bf5a73f8ec19d165fc61487bed
Ankan Basu <abasu@harmonia.com>
2017-01-23 20:10:27 -0500

    Some files added to to working initial setup.

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,8 +5,12 @@
 <body>
 	<h1>Hello World</h1>
 <?php
-
-echo 'Hello World'
+	require_once('/var/www/html/php/itp240/htdocsInclude/MyApplicationFramework/initialize.php');
+	require_once('/var/www/html/php/itp240/htdocsInclude/MyApplicationFramework/view/pageHeader.php');
+	require_once('/var/www/html/php/itp240/htdocsInclude/MyApplicationFramework/view/pageFooter.php');
+	echo $_SERVER['DOCUMENT_ROOT'];
+	echo "<br>";
+	echo 'Hello World';
 
 ?>
 
