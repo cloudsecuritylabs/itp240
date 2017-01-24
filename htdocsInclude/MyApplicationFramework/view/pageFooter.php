d07cac4c04ceffb3b67ecebe3e46d4583ee27bad
Ankan Basu <abasu@harmonia.com>
2017-01-23 20:09:25 -0500

    Working initial framework created. need to add some bootstrap files next.

---
+++ htdocsInclude/MyApplicationFramework/view/pageFooter.php
@@ -0,0 +1,7 @@
+
+	<h1>Footer</h1>	
+	<hr>
+	</div>
+</body>
+</html>
+
---
+++ htdocsInclude/MyApplicationFramework/view/pageHeader.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+	<title>My First Web Application Framework</title>
+	<link rel="shortcut icon" href="<?php echo WEB_ROOT ?>images/favicon.png" />
+	<!---****************************************-->
+	<!--styles									 -->
+	<link href="<?php echo WEB_ROOT ?>bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="<?php echo WEB_ROOT ?>css/bootstrapmodifications.css" rel="stylesheet" type="text/css">
+	<link href="<?php echo WEB_ROOT ?>css/site.css" rel="stylesheet" type="text/css">
+	<!---****************************************-->
+	<!--scripts									 -->
+	<script src="<?php echo WEB_ROOT ?>javascript/jquery-3.1.1.min.js"  type="text/javascript"></script>
+	<script src="<?php echo WEB_ROOT ?>bootstrap/js/bootstrap.min.js"  type="text/javascript"></script>
+
+
+</head>
+
+<body>
+	<nav></nav>
+	<div id='pageWrapper'>
+		<h1>Header</h1>
+		<hr>
+
