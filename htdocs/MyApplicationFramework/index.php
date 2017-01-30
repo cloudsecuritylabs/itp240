b949be10ca22b58b38acf0bc15801f091d7b0856
Ankan Basu <abasu@harmonia.com>
2017-01-29 22:53:30 -0500

    * Updated all of the pages to use bootstrap classes. * Perform testing of css files to work properly. * Perform testing of all include php files.

--- htdocs/MyApplicationFramework/index.php
+++ htdocs/MyApplicationFramework/index.php
@@ -1,25 +1,38 @@
 <html>
-<head>
-	<title>My Title</title>
-</head>
-<body>
-	<h1>Hello World</h1>
+
 <?php
 	##Get the initilize file
-	 echo "</h2>Get initialize file from htdocsIncludes</h2>";
+	 // echo "</h2>Get initialize file from htdocsIncludes</h2>";
 	require_once('/var/www/html/php/itp240/htdocsIncludes/MyApplicationFramework/initialize.php');
 	##Get the page header information
-	echo "<h2>Get pageHeader file from htdocsIncludes</h2>";
+	// echo "<h2>Get pageHeader file from htdocsIncludes</h2>";
 	require_once('/var/www/html/php/itp240/htdocsIncludes/MyApplicationFramework/view/pageHeader.php');
+
+?>
+
+<!-- main container should go here -->
+<div class="container">
+	<h1>All is working</h1>
+	<p>This is the main area to add context..This is the main area to add context..This is the main area to add context..This is the main area to add context..This is the main area to add context..This is the main area to add context..This is the main area to add context..
+	This is the main area to add context..This is the main area to add context..This is the main area to add context..This is the main area to add context..This is the main area to add context..
+	This is the main area to add context..This is the main area to add context..This is the main area to add context..This is the main area to add context..
+	This is the main area to add context..This is the main area to add context..This is the main area to add context..This is the main area to add context..This is the main area to add context..
+	This is the main area to add context..This is the main area to add context..This is the main area to add context..This is the main area to add context..
+	</p>
+
+</div>
+
+
+<?php
 	##get the page footer information.
-	echo "<h2>Get pageFooter file from htdocsIncludes</h2>";
+	// echo "<h2>Get pageFooter file from htdocsIncludes</h2>";
 	require_once('/var/www/html/php/itp240/htdocsIncludes/MyApplicationFramework/view/pageFooter.php');
 	
 	##print the document root of the server.
-	echo "<br>";
-	echo $_SERVER['DOCUMENT_ROOT'];
-	echo "<br>";
-	echo 'Hello World';
+	// echo "<br>";
+	// echo $_SERVER['DOCUMENT_ROOT'];
+	// echo "<br>";
+	// echo 'Hello World';
 
 ?>
 
